cbe04f3ab8b799cd9f0f4b407dc5699753f1af2a
hoangle96 <lebahoang010296@gmail.com>
2019-04-07 00:21:37 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='analyze reviews of big tech companies',
+    author='hoangle',
+    license='MIT',
+)
